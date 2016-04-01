23abb92edaa5aedf4f83c3d4eb11b0ce25b1d22f
Vladislav Petrov <electronicchest@gmail.com>
2016-04-02 00:40:18 +0300

    Add currency

---
+++ app/models/currency.rb
@@ -0,0 +1,13 @@
+class Currency
+  TOKEN = ENV['XIGNITE_TOKEN']
+  SITE = 'http://globalcurrencies.xignite.com'
+
+  def self.get(symbol = 'USDRUB', date: Date.current)
+    uri = URI("#{SITE}/xGlobalCurrencies.json/GetRealTimeRate")
+    uri.query = "Symbol=#{symbol}&_token=#{TOKEN}&Date=#{date.strftime('%m/%d/%Y')}"
+
+    response = Net::HTTP.get(uri)
+
+    format '%.2f', JSON.parse(response)['Ask']
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'home#index', as: :home
-
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
+
+  root to: 'home#index', as: :home
 end
